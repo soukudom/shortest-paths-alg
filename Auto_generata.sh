daca470be9b8d9420b2ef5bd8a6951b4169c7698
Dominik Soukup <soukupdo@gmail.com>
2017-03-21 21:05:33 +0100

    new test data

--- Auto_generata.sh
+++ Auto_generata.sh
@@ -1,10 +1,15 @@
 #! /bin/bash
 
 
-./GeneratorWithRating.sh 1500nodes10edges task1/data/nodes1500Edge10 #nodes\=100AvgEdge\=25 task1/data/nodes1000Edge25
-./GeneratorWithRating.sh 1500nodes50edges task1/data/nodes1500Edge50 #nodes\=100AvgEdge\=25 task1/data/nodes1000Edge25
-./GeneratorWithRating.sh 1500nodes100edges task1/data/nodes1500Edge100 #nodes\=100AvgEdge\=25 task1/data/nodes1000Edge25
-./GeneratorWithRating.sh 1500nodes500edges task1/data/nodes1500Edge500 #nodes\=100AvgEdge\=25 task1/data/nodes1000Edge25
-echo "25 done"
+./GeneratorWithRating.sh 2000nodes100Edges data/nodes2000Edge100 #nodes\=100AvgEdge\=25 task1/data/nodes1000Edge25
+echo "1"
+./GeneratorWithRating.sh 2000nodes300Edges data/nodes2000Edge300 #nodes\=100AvgEdge\=25 task1/data/nodes1000Edge25
+echo "2"
+./GeneratorWithRating.sh 2000nodes500Edges data/nodes2000Edge500 #nodes\=100AvgEdge\=25 task1/data/nodes1000Edge25
+./GeneratorWithRating.sh 2000nodes700Edges data/nodes2000Edge700 #nodes\=100AvgEdge\=25 task1/data/nodes1000Edge25
+echo "3"
+./GeneratorWithRating.sh 2000nodes1000Edges data/nodes2000Edge1000 #nodes\=100AvgEdge\=25 task1/data/nodes1000Edge25
+echo "4"
+#echo "25 done"
 #./GeneratorWithRating.sh nodes\=100AvgEdge\=75 task1/data/nodes1000Edge75
 #echo DONE!!
