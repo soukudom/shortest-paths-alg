3d28d49a03d522603ced9496596e92e1dc4d6c44
Dominik Soukup <soukupdo@gmail.com>
2017-03-21 22:48:49 +0100

    summary report was written

--- Auto_generata.sh
+++ Auto_generata.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
 
-./GeneratorWithRating.sh 2000nodes100Edges data/nodes2000Edge100 #nodes\=100AvgEdge\=25 task1/data/nodes1000Edge25
+./GeneratorWithRating.sh 1500nodes10Edges data/nodes1500Edge10 #nodes\=100AvgEdge\=25 task1/data/nodes1000Edge25
 echo "1"
-./GeneratorWithRating.sh 2000nodes300Edges data/nodes2000Edge300 #nodes\=100AvgEdge\=25 task1/data/nodes1000Edge25
+./GeneratorWithRating.sh 1500nodes50Edges data/nodes1500Edge50 #nodes\=100AvgEdge\=25 task1/data/nodes1000Edge25
 echo "2"
-./GeneratorWithRating.sh 2000nodes500Edges data/nodes2000Edge500 #nodes\=100AvgEdge\=25 task1/data/nodes1000Edge25
-./GeneratorWithRating.sh 2000nodes700Edges data/nodes2000Edge700 #nodes\=100AvgEdge\=25 task1/data/nodes1000Edge25
+./GeneratorWithRating.sh 1500nodes100Edges data/nodes1500Edge100 #nodes\=100AvgEdge\=25 task1/data/nodes1000Edge25
+./GeneratorWithRating.sh 1500nodes500Edges data/nodes1500Edge500 #nodes\=100AvgEdge\=25 task1/data/nodes1000Edge25
 echo "3"
-./GeneratorWithRating.sh 2000nodes1000Edges data/nodes2000Edge1000 #nodes\=100AvgEdge\=25 task1/data/nodes1000Edge25
+./GeneratorWithRating.sh 1500nodes700Edges data/nodes1500Edge700 #nodes\=100AvgEdge\=25 task1/data/nodes1000Edge25
 echo "4"
 #echo "25 done"
 #./GeneratorWithRating.sh nodes\=100AvgEdge\=75 task1/data/nodes1000Edge75
--- task2/dijkstra.cpp
+++ task2/dijkstra.cpp
@@ -224,23 +224,23 @@ int main( int argc, const char* argv[] )
     //start of measuring 
     double wall0 = get_wall_time();
     double cpu0  = get_cpu_time();
-    double start = omp_get_wtime();
+    //double start = omp_get_wtime();
 
     dijkstraAll();
 
     //end of measuring
     double wall1 = get_wall_time();
     double cpu1  = get_cpu_time();
-    double end = omp_get_wtime( );  
+  //  double end = omp_get_wtime( );  
     
 
     //prints results
-    for(int i = 0; i< nodes; i++){
+/*    for(int i = 0; i< nodes; i++){
         printDijkstra(i);
-    }
+    }*/
     deleteMemory();
     cout << "Wall Time = " << wall1 - wall0 << endl;
     cout << "CPU Time  = " << cpu1  - cpu0  << endl;
-    cout << "OpenMP Time = " << end - start << endl;
+//    cout << "OpenMP Time = " << end - start << endl;
     return 0;
 }
